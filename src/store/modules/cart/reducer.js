f70f3ff6a4746a3211fd30f8d085c594dcfa7359
Alex Tavella <alextavella@outlook.com>
2019-07-30 17:01:35 -0300

    Provide amount product

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -1,15 +1,21 @@
-export default function cart(state = [], action) {
-  // console.log(state);
+import produce from 'immer';
 
+export default function cart(state = [], action) {
   switch (action.type) {
     case 'ADD_TO_CART':
-      return [
-        ...state,
-        {
-          ...action.product,
-          amount: 1,
-        },
-      ];
+      return produce(state, draft => {
+        const productIndex = draft.findIndex(
+          item => item.id === action.product.id
+        );
+        if (productIndex >= 0) {
+          draft[productIndex].amount += 1;
+        } else {
+          draft.push({
+            ...action.product,
+            amount: 1,
+          });
+        }
+      });
     default:
       return state;
   }
