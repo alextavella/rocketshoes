6a6bb88319d98993526a1fac57a25b3be6182b80
Alex Tavella <alextavella@outlook.com>
2019-07-30 16:12:32 -0300

    Provide home and cart page

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { MdShoppingBasket } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container, Cart } from './styles';
+
+import logo from '../../assets/images/logo.svg';
 
 export default function Header() {
   return (
-    <div />
+    <Container>
+      <Link to="/">
+        <img src={logo} alt="Rocketshoes" />
+      </Link>
+
+      <Cart to="/cart">
+        <div>
+          <strong>Meu Carrinho</strong>
+          <span>3 itens</span>
+        </div>
+        <MdShoppingBasket size={36} color="#FFF" />
+      </Cart>
+    </Container>
   );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import api from '../../services/api';
+import { formatPrice } from '../../utils/format';
+
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <div />;
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const loadProducts = async () => {
+      const response = await api.get('/products');
+      const data = response.data.map(product => ({
+        ...product,
+        productFormatted: formatPrice(product.price),
+      }));
+
+      setProducts(data);
+    };
+
+    loadProducts();
+  }, []);
+
+  return (
+    <ProductList>
+      {products.map((product, index) => (
+        <li key={index}>
+          <img src={product.image} alt={product.title} />
+          <strong>{product.title}</strong>
+          <span>{product.productFormatted}</span>
+          <button type="button">
+            <div>
+              <MdAddShoppingCart size={16} color="#FFF" />
+            </div>
+            <span>Adicionar ao carrinho</span>
+          </button>
+        </li>
+      ))}
+    </ProductList>
+  );
 }
